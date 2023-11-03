70a31fd3d15c568934df071ee7ba6e0364648638
diegokld30 <dfercalderon@poligran.edu.co>
2023-11-02 21:26:39 -0500

    A que no lo hace servir

---
+++ routes/web.php
@@ -0,0 +1,40 @@
+<?php
+
+use App\Http\Controllers\ImagenController;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\LogoutController;
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\RegisterController;
+use Illuminate\Support\Facades\Route;
+
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+Route::get('/', function () {
+    return view('principal');
+});
+Route::get('/register', [RegisterController::class, 'index'])->name('register');
+Route::post('/register', [RegisterController::class, 'store']);
+
+Route::get('/login', [LoginController::class, 'index'])->name('login');
+Route::post('/login', [LoginController::class, 'store']);
+
+Route::post('/logout', [LogoutController::class, 'store'])->name('logout');
+
+//Route::get('/muro', [PostController::class, 'index'])->name('post.index');
+Route::get('/{user:username}', [PostController::class, 'index'])->name('post.index');
+
+Route::get('/posts/create', [PostController::class, 'create'])->name('post.create');
+
+Route::post('/imagenes', [ImagenController::class, 'store'])->name('imagenes.store');
+
+
+
